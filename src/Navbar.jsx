d3401d642daaf54bced3108c6b82019770801a1e
SabPat702 <SabPat702@hengersor.hu>
2025-02-17 12:19:37 +0100

    menü elkészítése
    css formázása

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,10 +5,12 @@ const Navbar = (() => {
 
     return (
         <div>
-            <nav>
-                <Link to='/'>Fő oldal</Link>
-                <NavLink to='/ermesek'>Az olimpia érmesei</NavLink>
-                <NavLink to='/mermesek'>Az olimpia magyar érmesei</NavLink>
+            <nav className="pa3 pa4-ns">
+                <Link className="link dim black b f1 f-headline-ns tc db mb3 mb4-ns" to='/'>Fő oldal</Link>
+                <div className="tc pb3">
+                    <NavLink className="link dim gray f6 f5-ns dib mr3" to='/ermesek'>Az olimpia érmesei</NavLink>
+                    <NavLink className="link dim gray f6 f5-ns dib mr3" to='/mermesek'>Az olimpia magyar érmesei</NavLink>
+                </div>
             </nav>
         </div>
     )
--- src/oldalak/Ermesek.jsx
+++ src/oldalak/Ermesek.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Ermesek = (() =>{
 
     return(
-        <div>
+        <div className="pa3 pa5-ns">
             <h1>Az olimpia érmesei</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae dolorum maiores eum consequuntur incidunt. Similique aut consequatur eius molestiae culpa modi, impedit sapiente ad rem assumenda repellendus laudantium non ea illum deserunt et cupiditate perspiciatis rerum tenetur? Laboriosam, qui ad impedit, dicta, sunt magnam fuga nam sed officia animi est temporibus assumenda! Accusantium iure incidunt nesciunt neque sint, aperiam iste, at dicta aliquam quam perspiciatis repudiandae ut quaerat, magnam quis dignissimos tempore culpa voluptatum corporis animi? Fuga temporibus ab nam nesciunt omnis facilis et hic ducimus amet quidem nulla, tempora repellendus rem ipsum illum reiciendis laboriosam velit at rerum itaque.</p>
+            <p className="f4 lh-copy measure">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae dolorum maiores eum consequuntur incidunt. Similique aut consequatur eius molestiae culpa modi, impedit sapiente ad rem assumenda repellendus laudantium non ea illum deserunt et cupiditate perspiciatis rerum tenetur? Laboriosam, qui ad impedit, dicta, sunt magnam fuga nam sed officia animi est temporibus assumenda! Accusantium iure incidunt nesciunt neque sint, aperiam iste, at dicta aliquam quam perspiciatis repudiandae ut quaerat, magnam quis dignissimos tempore culpa voluptatum corporis animi? Fuga temporibus ab nam nesciunt omnis facilis et hic ducimus amet quidem nulla, tempora repellendus rem ipsum illum reiciendis laboriosam velit at rerum itaque.</p>
         </div>
     )
 
